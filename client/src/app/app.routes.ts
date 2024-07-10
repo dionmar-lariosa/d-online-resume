82a6828055ca4381a31490979f330a14adeec06b
dionmar-lariosa <main.dionmar@gmail.com>
2024-07-10 23:30:04 +0800

    base setup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthComponent } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'home', component: LandingPageComponent },
+  { path: 'auth', title: 'auth', component: AuthComponent },
+];
