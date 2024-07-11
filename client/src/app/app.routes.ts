d3d6e436741365ecd2385e2dd803b1371b805d44
dionmar-lariosa <main.dionmar@gmail.com>
2024-07-11 11:37:05 +0800

    guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', title: 'Home', component: LandingPageComponent },
   { path: 'auth', title: 'Auth', component: AuthComponent },
-  { path: 'dashboard', title: 'Dashboard', component: DashboardComponent },
+  {
+    canActivate: [authGuard],
+    path: 'dashboard',
+    title: 'Dashboard',
+    component: DashboardComponent,
+  },
 ];
