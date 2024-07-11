52df743bc546c982117cf310de8fa95bcac5106a
dionmar-lariosa <main.dionmar@gmail.com>
2024-07-11 13:05:11 +0800

    page title

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,13 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router, RouterLink } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterLink,
+} from '@angular/router';
 import { AuthStore } from '../auth/auth.store';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { NgIf } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -26,9 +32,33 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
   authStore = inject(AuthStore);
   isOpen = true;
+  pageTitle = '';
+
+  ngOnInit() {
+    this.setPageTitle();
+  }
+
+  setPageTitle() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((r) => {
+          while (r.firstChild) {
+            r = r.firstChild;
+          }
+          return r;
+        }),
+        filter((r) => r.outlet === 'primary')
+      )
+      .subscribe(
+        (r) =>
+          (this.pageTitle = (r.snapshot.routeConfig?.title as string) || '')
+      );
+  }
 
   onLogout() {
     localStorage.removeItem('user');
